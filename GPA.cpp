9f52ce153afd1a3a52c6cd72cdfb542434b7334f
MaxKreitzer <201699@pennmanor.net>
2021-04-21 13:06:15 -0400

    Figured out how to add the gpa based off whether CP or a letter is identified.

--- GPA.cpp
+++ GPA.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
 int main() {
   ifstream file;
   string data;
+  float gpa = 5.5;
   file.open("major.gpa.txt");
+//  getline(file, data);
   while(getline(file, data)){
-    cout << data;
-    cout << "\n";
-  }
 
+    istringstream iss(data);
+    do{
+      string subs;
+      iss >> subs;
+      cout << subs << "\n";
+      if (subs == "CP"){
+        gpa = gpa - 1.5;
+      }
+    }
+    while(iss);
+//    cout << data;
+//    cout << "\n";
+  }
+  cout << gpa;
+  cout << "\n";
   file.close();
   return 0;
 }
