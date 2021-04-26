8dc41d8ffd245ff9a51be0614d51a85192eac918
MaxKreitzer <201699@pennmanor.net>
2021-04-26 08:12:57 -0400

    save

--- GPA.cpp
+++ GPA.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 using namespace std;
 int main() {
   ifstream file;
   string data;
-  float gpa = 5.5;
+  float gpa = 0;
   file.open("major.gpa.txt");
 //  getline(file, data);
   while(getline(file, data)){
-
     istringstream iss(data);
     do{
       string subs;
       iss >> subs;
       cout << subs << "\n";
       if (subs == "CP"){
-        gpa = gpa - 1.5;
+        gpa = gpa + 0;
+      if (subs == "HN"){
+        gpa = gpa + 0.5;
+      }
+      if (subs == "AP"){
+        gpa = gpa + 1;
+      }
+      if (subs == "A"){
+        gpa = gpa + 4;
+      }
+      if (subs == "B"){
+        gpa = gpa + 3;
+      }
+      if (subs == "C"){
+        gpa = gpa + 2;
+      }
+      if (subs == "D"){
+        gpa = gpa + 1;
       }
     }
     while(iss);
