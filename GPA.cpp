fe9b07a1adc2758d67d02546838522948173e40c
MaxKreitzer <201699@pennmanor.net>
2021-04-26 08:59:29 -0400

    Added decription of code. Project basically completed. May try to add UI later.

--- GPA.cpp
+++ GPA.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+// fstream helps to open a .txt file to read it
 #include <fstream>
+// sstream is used for its library in seperating strings by spaces
 #include <sstream>
-#include <vector>
 using namespace std;
+
 int main(){
 
+// initiates the .txt file and declares the gpa initially as 0 as well as the
+// total of classes taken as 0 called varbiable room
   ifstream file;
   string data;
   float gpa = 0;
   float room = 0;
 
+// opens the .txt file and begins reading line by getline
+// also istringstream iss begins seperating the strings by their spaces and puts
+// them on seperate readable lines
   file.open("major.gpa.txt");
   while(getline(file, data)){
     istringstream iss(data);
@@ -17,6 +24,9 @@ int main(){
       string subs;
       iss >> subs;
 
+// From here depending on the type of class a 0, 0.5, 1, or 1.5 is assigned to
+// the gpa.
+// Also depending on the letter a nummber, 0, 1, 2, 3, or 4 is added to the gpa
       if (subs == "CP"){
         gpa = gpa + 0;
         room = room + 1;
@@ -50,10 +60,13 @@ int main(){
       }
     }
 
+// continues to run the program that seperates strings by spaces
     while(iss);
 
     }
 
+// finally gpa is divided by the total amount of classes and final gpa is printed
+// also the .txt file is closed just to make sure it doesn't get edited
   cout << (gpa / room);
   cout << "\n";
   file.close();
